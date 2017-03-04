aff1d750d392d46023973ef801333e644e22dfe7
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2017-03-04 23:25:35 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var express 			= require("express"),
+app 					= express(),
+//to get APIS
+request 				= require("request"),
+//to link css
+path 					= require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function(req, res) {	
+	res.render("search");
+});
+
+app.get("/results", function(req, res) {
+	var query = req.query.search;
+	var url = "http://www.omdbapi.com/?t=" + query +"&y=&plot=short&r=json&tomatoes=true";
+	console.log(url);
+	request(url, function(err, response, body) {
+		if(!err && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			res.render("search", {data: data});
+		}
+	});
+});
+
+app.get("*", function(req, res) {	
+	res.render("search");
+});
+
+
+app.listen(3000, function() {
+	console.log('Express server started on port 3000');
+});
+
